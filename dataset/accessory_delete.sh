237b6ff94d8eb17867d7838feab210daf2571b64
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-05-18 22:23:47 +0900

    Add files via upload

--- dataset/accessory_delete.sh
+++ dataset/accessory_delete.sh
@@ -4,6 +4,8 @@ for file1 in /home/foscar/다운로드/High_Resolution/*
 do
 	for file2 in $file1/*
 	do
+		find . -name 'S002' -type d -exec rm -rf {} \;
+		find . -name 'S003' -type d -exec rm -rf {} \;
 		find . -name 'S004' -type d -exec rm -rf {} \;
 		find . -name 'S005' -type d -exec rm -rf {} \;
 		find . -name 'S006' -type d -exec rm -rf {} \;
